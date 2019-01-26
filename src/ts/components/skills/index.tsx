7a9b53dd8a6598befeae4fbec4d7d4c7a9fb63e9
Wavum <rl00@gmx.at>
2019-01-26 18:42:49 +0100

    Update skills with projects

--- src/ts/components/skills/index.tsx
+++ src/ts/components/skills/index.tsx
@@ -1,26 +1,20 @@
 import React from "react";
-import styled from "styled-components";
 import Item from "./item";
 import Context from "./../../store";
-
-const SkillItem = styled(Item)`
-  padding: 51px 0;
-  margin: -1px 0;
-  border-top: 1px solid ${props => props.theme.colors.greyLight};
-  border-bottom: 1px solid ${props => props.theme.colors.greyLight};
-  color: ${props => props.theme.colors.greyLight};
-`;
+import { Accordions } from "./../../shared";
 
 interface Props extends React.HTMLProps<HTMLDivElement> { }
 
 export default (props: Props) => {
   return (
     <Context.Consumer>
-      {value => (
+      {data => (
         <div {...props}>
           <section className="section is-large">
             <div className="container">
-              {value.skills.map((skill, i) => <SkillItem key={i} name={skill.name} percentage={skill.knowledgeInPercentage} />)}
+              <Accordions>
+                {data.skills.map((skill, i) => <Item key={i} name={skill.name} percentage={skill.knowledgeInPercentage} projects={skill.projects} />)}
+              </Accordions>
             </div>
           </section>
         </div>
--- src/ts/components/skills/item/index.tsx
+++ src/ts/components/skills/item/index.tsx
@@ -1,25 +1,63 @@
 import React from "react";
+import { ExternalLink } from "react-feather";
+import styled from "./../../../theme";
+import { Accordion } from "./../../../shared";
+
+const Project = styled.div`
+    padding: 32px;
+  `;
 
 interface Props extends React.HTMLProps<HTMLDivElement> {
   name: string;
   percentage: number;
+  projects?: {
+    name: string;
+    description: string;
+    link: string;
+  }[];
 }
 
 export default (props: Props) => {
-  const { name, percentage, ...newProps } = props;
+  const { name, percentage, projects, ...newProps } = props;
 
   return (
     <div {...newProps}>
-      <div className="columns is-vcentered">
-        <div className="column is-3">
-          <p className="title">{name}</p>
-        </div>
-        <div className="column">
-          <div className="tooltip is-tooltip-right is-tooltip-top-touch" data-tooltip={percentage + "%"}>
-            <progress className="progress is-large is-primary" value={percentage} max="100" />
+      <Accordion header={(
+        <div className="columns is-vcentered">
+          <div className="column is-3">
+            <p className="title">{name}</p>
           </div>
-        </div>
-      </div>
+          <div className="column">
+            <div className="tooltip is-tooltip-right is-tooltip-top-touch" data-tooltip={percentage + "%"}>
+              <progress className="progress is-large is-primary" value={percentage} max="100" />
+            </div>
+          </div>
+        </div>)}>
+        {
+          projects ?
+            projects.map((project, i) => (
+              <Project key={i}>
+                <div className="columns">
+                  <div className="column is-narrow">Image</div>
+                  <div className="column">
+                    <div className="content">
+                      <strong>{project.name}</strong><br />
+                      {project.description}
+                    </div>
+                  </div>
+                  <div className="column is-narrow">
+                    <a target="_blank" href={project.link}>
+                      <span className="icon">
+                        <ExternalLink />
+                      </span>
+                    </a>
+                  </div>
+                </div>
+              </Project>
+            )) :
+            null
+        }
+      </Accordion>
     </div>
-  );
+  )
 }
