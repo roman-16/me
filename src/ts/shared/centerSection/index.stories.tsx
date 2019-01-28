d9c907fa907bd96d12aff0ffec4671a73bfc3a87
Roman <roman@lerchster.dev>
2019-01-28 21:41:46 +0100

    Add only horizontal to centerSection

--- src/ts/shared/centerSection/index.stories.tsx
+++ src/ts/shared/centerSection/index.stories.tsx
@@ -6,42 +6,51 @@ import Section from ".";
 const stories = storiesOf("CenterSection", module);
 
 stories.add("some text", () => (
-    <Section isFullheight={boolean("is fullheight", true)}>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>It can even be another Collapsible component. Check out the next section!</p>
-    </Section>
-  ));
+  <Section
+    isFullheight={boolean("is fullheight", true)}
+    onlyHorizontal={boolean("only horizontal", false)}
+  >
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>It can even be another Collapsible component. Check out the next section!</p>
+  </Section>
+));
 
 stories.add("some image", () => (
-    <Section isFullheight={boolean("is fullheight", true)}>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <img src="http://lorempixel.com/400/400/cats/" />
-    </Section>
-  ));
+  <Section
+    isFullheight={boolean("is fullheight", true)}
+    onlyHorizontal={boolean("only horizontal", false)}
+  >
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <img src="http://lorempixel.com/400/400/cats/" />
+  </Section>
+));
 
 stories.add("much content", () => (
-    <Section isFullheight={boolean("is fullheight", true)}>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <img src="http://lorempixel.com/400/400/cats/" />
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <img src="http://lorempixel.com/400/400/cats/" />
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <img src="http://lorempixel.com/400/400/cats/" />
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-    </Section>
-  ));
+  <Section
+    isFullheight={boolean("is fullheight", true)}
+    onlyHorizontal={boolean("only horizontal", false)}
+  >
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <img src="http://lorempixel.com/400/400/cats/" />
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <img src="http://lorempixel.com/400/400/cats/" />
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <img src="http://lorempixel.com/400/400/cats/" />
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+  </Section>
+));
