b96723d9d551e4b0b5c9dd18834bd3c702df21c4
Roman <roman@lerchster.dev>
2019-02-02 18:27:38 +0100

    Add sociallinks in callToAction

--- src/ts/components/hero/CallToAction.tsx
+++ src/ts/components/hero/CallToAction.tsx
@@ -1,26 +1,65 @@
 import React from "react";
-import { ArrowRight, GitHub } from "react-feather";
+import * as Icons from "react-feather";
+import styled from "./../../theme";
+
+const Container = styled.div`
+  float: left;
+  background-color: ${props => props.theme.colors.greyLighter};
+  border-radius: 24px;
+`;
+
+const IconContainer = styled.div`
+  display: flex;
+  padding: 0px 8px;
+`;
+
+const Icon = styled.div`
+  margin: 8px;
+`;
 
 interface Props extends React.HTMLProps<HTMLDivElement> {
   text: string;
+  socialLinks: {
+    name: string;
+    icon: string;
+    url: string;
+  }[];
 }
 
 export default (props: Props) => {
-  const { text, ...newProps } = props;
+  const { text, socialLinks, ...newProps } = props;
 
   return (
     <div {...newProps}>
-      <a className="button is-primary is-medium is-rounded" href="#skills">
-        <span>{text}</span>
-        <span className="icon">
-          <ArrowRight />
-        </span>
-      </a>
-      <a className="button is-rounded">
-        <span className="icon">
-          <GitHub size="20px" />
-        </span>
-      </a>
+      <div className="columns is-gapless">
+        <Container className="column is-narrow">
+          <a className="button is-primary is-medium is-rounded" href="#skills">
+            <span>{text}</span>
+            <span className="icon">
+              <Icons.ArrowRight />
+            </span>
+          </a>
+          <IconContainer>
+            {
+              socialLinks.map((link, i) => {
+                const SvgIcon = Icons[link.icon as keyof typeof Icons];
+
+                return (
+                  <Icon>
+                    <div className="tooltip is-tooltip-bottom" data-tooltip={link.name}>
+                      <a rel="noopener" target="_blank" href={link.url}>
+                        <span className="icon">
+                          <SvgIcon size="24px" />
+                        </span>
+                      </a>
+                    </div>
+                  </Icon>
+                );
+              })
+            }
+          </IconContainer>
+        </Container>
+      </div>
     </div>
   );
 }
