3352209264fbc8a03b795218c25fbaf46150ac1e
Wavum <rl00@gmx.at>
2019-02-03 10:52:47 +0100

    Link: open only http url in new tab

--- src/ts/shared/Link/index.tsx
+++ src/ts/shared/Link/index.tsx
@@ -3,7 +3,9 @@ import React from "react";
 interface Props extends React.HTMLProps<HTMLAnchorElement> { }
 
 export default (props: Props) => {
+  let target = props.href && props.href.startsWith("http") ? "_blanks" : "";
+
   return (
-    <a rel="noopener" target="_blank" {...props} />
+    <a rel="noopener" target={target} {...props} />
   );
 }
