295d88b8d65147a1395585e6da5551cd9b2bc0f6
Wavum <rl00@gmx.at>
2019-02-03 12:25:43 +0100

    Rework about me section

--- src/ts/components/AboutMe/index.tsx
+++ src/ts/components/AboutMe/index.tsx
@@ -10,14 +10,14 @@ export default (props: Props) => {
       <div className="content is-medium">
         I grew up on a farm in the mountains of upper <Link href="https://en.wikipedia.org/wiki/Carinthia">carinthia</Link> (Austria).<br />
         <br />
-        To the surprise of my parents I always enjoyed spending much more time on my computer than working and helping at home.
-        But I never thought programming was something I'd enjoy.
-        I have always regarded programming as something very complicated and prehaps a little boring.
-        In high school I came for the first time in contact with programming (HTML, CSS) and it suited me really well.
+        To the surprise of my parents, I always enjoyed spending more time learning about computers than working at our farm.
+        I never thought programming was something I would enjoy.
+        At first I have always regarded programming as something very complicated and prehaps a little boring.
+        In high school I came in contact, for the first time, with programming (HTML, CSS) and it suited me really well.
         I began an apprenticeship as a software developer at <Link href="https://itlabs.at">IT-L@B</Link> in <Link href="https://en.wikipedia.org/wiki/Villach">Villach</Link>.
         There I learned the basics of C# and web development.
-        Since then I've been constantly learning new technologies.
-        In my second year of apprenticeship, <Link href="https://strabag.com">Strabag</Link> in <Link href="https://en.wikipedia.org/wiki/Spittal_an_der_Drau">Spittal</Link> hired me, where I finished my apprenticeship and where I am currently working.
+        Since then I have been constantly learning new technologies.
+        In my second year of apprenticeship, <Link href="https://strabag.com">Strabag</Link> in <Link href="https://en.wikipedia.org/wiki/Spittal_an_der_Drau">Spittal</Link> hired me, where I finished my apprenticeship and I am currently working at.
       </div>
       <Link className="button is-primary is-medium is-rounded" href="mailto:rl00@gmx.at">
         <span>Contact me</span>
