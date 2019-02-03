9545cd3d97cc8f7aabe30b5682d82392485d92d1
Wavum <rl00@gmx.at>
2019-02-03 11:35:30 +0100

    Some housekeeping

--- src/ts/shared/Accordion/index.stories.tsx
+++ src/ts/shared/Accordion/index.stories.tsx
@@ -5,44 +5,44 @@ import Accordion from ".";
 const stories = storiesOf("Accordion", module);
 
 stories.add("some text", () => (
-    <Accordion header="Head">
-      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
-    </Accordion>
-  ));
+  <Accordion header="Head">
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+  </Accordion>
+));
 
 stories.add("some image", () => (
-    <Accordion header="Head">
-      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
-      <img src="http://lorempixel.com/400/400/cats/" />
-    </Accordion>
-  ));
+  <Accordion header="Head">
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+    <img src="http://lorempixel.com/400/400/cats/" />
+  </Accordion>
+));
 
 stories.add("extensive header", () => (
-    <Accordion header={(
-        <>
-          <h1>Head</h1>
-          <button onClick={(ev) => {
-              ev.stopPropagation();
-              alert("Hi");
-            }}>
-            Say hi!
-          </button>
-        </>
-      )}>
+  <Accordion header={(
+      <>
+        <h1>Head</h1>
+        <button onClick={(ev) => {
+            ev.stopPropagation();
+            alert("Hi");
+          }}>
+          Say hi!
+        </button>
+      </>
+    )}>
+    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+    <img src="http://lorempixel.com/400/400/cats/" />
+  </Accordion>
+));
+
+stories.add("multiple accordions", () => (
+  <div>
+    <Accordion header={"Head 1"}>
       <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
       <img src="http://lorempixel.com/400/400/cats/" />
     </Accordion>
-  ));
-
-stories.add("multiple accordions", () => (
-    <div>
-      <Accordion header={"Head 1"}>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
-        <img src="http://lorempixel.com/400/400/cats/" />
-      </Accordion>
-      <Accordion header={"Head 2"}>
-        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
-        <img src="http://lorempixel.com/400/400/cats/" />
-      </Accordion>
-    </div>
-  ));
+    <Accordion header={"Head 2"}>
+      <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+      <img src="http://lorempixel.com/400/400/cats/" />
+    </Accordion>
+  </div>
+));
--- src/ts/shared/Collapsible/index.stories.tsx
+++ src/ts/shared/Collapsible/index.stories.tsx
@@ -6,15 +6,15 @@ import Collapsible from ".";
 const stories = storiesOf("Collapsible", module);
 
 stories.add("some text", () => (
-    <Collapsible isOpen={boolean("is open", true)}>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <p>It can even be another Collapsible component. Check out the next section!</p>
-    </Collapsible>
-  ));
+  <Collapsible isOpen={boolean("is open", true)}>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <p>It can even be another Collapsible component. Check out the next section!</p>
+  </Collapsible>
+));
 
 stories.add("some image", () => (
-    <Collapsible isOpen={boolean("is open", true)}>
-      <p>This is the collapsible content. It can be any element or React component you like.</p>
-      <img src="http://lorempixel.com/400/400/cats/" />
-    </Collapsible>
-  ));
+  <Collapsible isOpen={boolean("is open", true)}>
+    <p>This is the collapsible content. It can be any element or React component you like.</p>
+    <img src="http://lorempixel.com/400/400/cats/" />
+  </Collapsible>
+));
