1d979a0c650c834268bd8619aed55a2db6a8c839
Roman <rl16@posteo.net>
2022-04-19 22:25:11 +0200

    Switch to simpler page

--- src/ts/shared/index.ts
+++
@@ -1,6 +0,0 @@
-import Accordion from "./Accordion";
-import CenterSection from "./CenterSection";
-import Collapsible from "./Collapsible";
-import Link from "./Link";
-
-export { Accordion, CenterSection, Collapsible, Link };
