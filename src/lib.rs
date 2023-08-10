1902fd1b9661e8d20578c49e8347ba4529b9c05b
Roman <rl16@posteo.net>
2023-08-10 23:26:26 +0200

    add basic leptos site

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use leptos::*;
+
+#[component]
+pub fn App(cx: Scope) -> impl IntoView {
+    let (count, set_count) = create_signal(cx, 0);
+
+    view! { cx,
+        <button
+            on:click=move |_| {
+                set_count(3);
+            }
+        >
+            "Click me: "
+            {move || count.get()}
+        </button>
+    }
+}
