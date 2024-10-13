fc2264be7cd16b5f94018fdbcf2d15dd7c78be31
Roman <roman@lerchster.dev>
2024-10-13 19:08:46 +0200

    Fix toolchain bug

--- flake.nix
+++ flake.nix
@@ -16,17 +16,23 @@
   }:
     flake-utils.lib.eachDefaultSystem (
       system: let
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-qwnTdNE6pxFTaulhZOfLb0dMiQwQWwfk42tCYEAFPHU=";
+        };
         pkgs = nixpkgs.legacyPackages.${system};
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            (fenix.packages.${system}.fromToolchainFile {
-              file = ./rust-toolchain.toml;
-              sha256 = "sha256-qwnTdNE6pxFTaulhZOfLb0dMiQwQWwfk42tCYEAFPHU=";
-            })
+            toolchain
             just
             trunk
           ];
+
+          # Certain Rust tools won't work without this
+          # This can also be fixed by using oxalica/rust-overlay and specifying the rust-src extension
+          # See https://discourse.nixos.org/t/rust-src-not-found-and-other-misadventures-of-developing-rust-on-nixos/11570/3?u=samuela. for more details.
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         };
       }
     );
