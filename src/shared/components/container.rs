6a82531ecc509a50655af8e9675c07d52c016b87
Roman <roman@lerchster.dev>
2024-11-12 23:31:43 +0100

    Add basic navbar

--- src/shared/components/container.rs
+++ src/shared/components/container.rs
@@ -1,12 +1,10 @@
 use leptos::prelude::*;
 
 #[component]
-pub fn Container() -> impl IntoView {
+pub fn Container(children: Children, #[prop(optional)] class: &'static str) -> impl IntoView {
     view! {
-      <div class="flex justify-center">
-        <div class="max-w-7xl w-full">
-          RL
+        <div class="flex justify-center ".to_owned() + class>
+            <div class="max-w-7xl w-full">{children()}</div>
         </div>
-      </div>
     }
 }
